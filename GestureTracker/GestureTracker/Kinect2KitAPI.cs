274cdea41fcaba143d88ce02ceceedc27a2b6fee
unknown <cjw21@st-andrews.ac.uk>
2015-07-13 23:00:19 +0100

    basic ui look

---
+++ GestureTracker/GestureTracker/Kinect2KitAPI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace GestureTracker
+{
+    class Kinect2KitAPI
+    {
+        public static string Server_Address { get; set; }
+
+        public static readonly string API_New_Session = "/session/new";
+        public static readonly string API_Acquire_Calibration = "calibration/acquire";
+        public static readonly string API_Resolve_Calibration = "calibration/resolve";
+        public static readonly string API_Start_Tracking = "/track/start";
+
+        public static string URL_For(string api)
+        {
+            return Kinect2KitAPI.Server_Address + api;
+        }
+
+        public static dynamic GetPOSTResponseJSON(string api)
+        {
+            return Kinect2KitAPI.GetPOSTResponseJSON(api, new List<KeyValuePair<string, string>>());
+        }
+
+        public static dynamic GetPOSTResponseJSON(string api, List<KeyValuePair<string, string>> values)
+        {
+            string url = Kinect2KitAPI.URL_For(api);
+            System.Diagnostics.Debug.WriteLine(url);
+            using (var client = new HttpClient())
+            {
+                var data = new FormUrlEncodedContent(values);
+                var response = client.PostAsync(url, data).Result;
+                return JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+            };
+        }
+    }
+}
---
+++ GestureTracker/GestureTracker/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Net.Http;
+
+namespace GestureTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private DrawingGroup kinectImageDrawingGroup;
+        private ImageSource kinectImageSource;
+
+        private string statusText;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string serverAddress = "localhost";
+        private int serverPort = 8000;
+        private string sessionName = "Test"; 
+
+        public MainWindow()
+        {
+            this.kinectImageDrawingGroup = new DrawingGroup();
+            this.kinectImageSource = new DrawingImage(this.kinectImageDrawingGroup);
+
+            this.DataContext = this;
+
+            this.InitializeComponent();
+
+            Kinect2KitAPI.Server_Address = "http://" + this.serverAddress + ":" + this.serverPort;
+
+            this.StatusText = "Running Gesture Tracker...";
+        }
+
+        public ImageSource KinectImageSource
+        {
+            get
+            {
+                return this.kinectImageSource;
+            }
+        }
+
+        public string StatusText
+        {
+            get
+            {
+                return this.statusText;
+            }
+
+            set
+            {
+                if (this.statusText != value)
+                {
+                    this.statusText = value;
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        private void Setup_Kinect2Kit_Server_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Setup_Kinects_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Setup_Session_Click(object sender, RoutedEventArgs e)
+        {
+            var values = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("name", this.sessionName)
+            };
+            dynamic response = Kinect2KitAPI.GetPOSTResponseJSON(Kinect2KitAPI.API_New_Session, values);
+            this.StatusText = "Setup session..." + response.message;
+        }
+
+        private void Calibration_Acquire_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic response = Kinect2KitAPI.GetPOSTResponseJSON(Kinect2KitAPI.API_Acquire_Calibration);
+            this.StatusText = "Start acquiring calibration..." + response.message;
+        }
+        private void Calibration_Resolve_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic response = Kinect2KitAPI.GetPOSTResponseJSON(Kinect2KitAPI.API_Resolve_Calibration);
+            this.StatusText = "Start resolving calibration..." + response.message;
+        }
+
+        private void Start_Tracking_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic response = Kinect2KitAPI.GetPOSTResponseJSON(Kinect2KitAPI.API_Start_Tracking);
+            this.StatusText = "Start Tracking..." + response.message;
+        }
+    }
+}
